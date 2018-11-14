2de474e79d4eec0d93b9c408302e9e5645d6e6f7
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-11-14 13:57:26 -0200

    Começo do POST

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ var port = 8080
 
 app.listen(port)
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongo.Server('localhost', 27017, {}),
+    {}
+)
+
 console.log('Servidor http está escutando na porta ' + port)
 
 app.get('/', function(req, res){
@@ -19,4 +25,11 @@ app.get('/', function(req, res){
     var resposta = ({ msg: 'Olá'})
     res.send(resposta)
 
+})
+
+app.post('/api', function(req, res){
+
+    var dados = req.body
+
+    res.send(dados)
 })
